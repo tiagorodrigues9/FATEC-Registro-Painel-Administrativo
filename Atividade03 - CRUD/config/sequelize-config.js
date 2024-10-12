c4a11f23e462191f6755a305ab5f48dd96d2355e
Tiago Rodrigues <160252876+tiagorodrigues9@users.noreply.github.com>
2024-10-12 10:29:18 -0300

    sequelize-config.js

--- Atividade03 - CRUD/config/sequelize-config.js
+++ Atividade03 - CRUD/config/sequelize-config.js
@@ -6,7 +6,7 @@ const connection = new Sequelize({
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: '25183030Atdf!', //alunos deixem a senha em branco
+    password: '25183030Atdf!', //Senha colocada referente ao meu MySQL
     database: 'loja',// comentem essa linha na primeira execucao da aplicacao
     timezone: '-03:00',
     logging: false
